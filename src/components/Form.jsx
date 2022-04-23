7f8fd84ac3ac249a6e2bf51572670ba3f349ee55
Leonardo Natera <lnatera27@gmail.com>
2022-04-23 15:58:10 -0500

    Feat: New form structure and env variables

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,97 @@
 import React from "react";
-import { firebase } from "firebase";
 
 const Form = () => {
     return (
-        <div className="form">
-        <form>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Last Name" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Phone" />
-            <input type="text" placeholder="Address" />
-            <input type="text" placeholder="City" />
-            <input type="text" placeholder="State" />
-            <button type="submit">Save</button>
-        </form>
-        </div>
+        <>
+            <div class="mt-10 sm:mt-0">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+
+                    {/** INFO */}
+                    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                        <div class="px-4 py-5 sm:px-6">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900">Users Information</h3>
+                            <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+                        </div>
+                        <div class="border-t border-gray-200">
+                            <dl>
+                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">Full name</dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Margot Foster</dd>
+                                </div>
+                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">Email address</dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">margotfoster@example.com</dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+
+                    {/** FORM */}
+                    <div class="mt-5 md:mt-0 md:col-span-2">
+                        <form action="#" method="POST">
+                            <div class="shadow overflow-hidden sm:rounded-md">
+                                <div class="px-4 py-5 bg-white sm:p-6">
+                                    <div class="grid grid-cols-6 gap-6">
+                                        <div class="col-span-6 sm:col-span-3">
+                                            <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
+                                            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-3">
+                                            <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
+                                            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-4">
+                                            <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
+                                            <input type="text" name="email-address" id="email-address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-3">
+                                            <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
+                                            <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                                <option>Colombia</option>
+                                                <option>United States</option>
+                                                <option>Mexico</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="col-span-6">
+                                            <label for="street-address" class="block text-sm font-medium text-gray-700">Street address</label>
+                                            <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                                            <label for="city" class="block text-sm font-medium text-gray-700">City</label>
+                                            <input type="text" name="city" id="city" autocomplete="address-level2" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                                            <label for="region" class="block text-sm font-medium text-gray-700">State / Province</label>
+                                            <input type="text" name="region" id="region" autocomplete="address-level1" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        </div>
+
+                                        <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                                            <label for="postal-code" class="block text-sm font-medium text-gray-700">Phone number</label>
+                                            <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hidden sm:block" aria-hidden="true">
+                <div class="py-5">
+                    <div class="border-t border-gray-200"></div>
+                </div>
+            </div>
+        </>
     );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCxx56OlmSQTtY91Z53ONNbv5dGPahKc4s",
-  authDomain: "epic-react-form.firebaseapp.com",
-  projectId: "epic-react-form",
-  storageBucket: "epic-react-form.appspot.com",
-  messagingSenderId: "228637389606",
-  appId: "1:228637389606:web:407132bd89e1f1bf9f07e7"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID
 };
 
 
